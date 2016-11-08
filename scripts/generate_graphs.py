c1830de0d180c9692b9f078567254790470ba378
Dylan Albrecht <dylan.alb@gmail.com>
2016-11-08 10:19:25 -0700

    updates graph script

--- scripts/generate_graphs.py
+++ scripts/generate_graphs.py
@@ -1,7 +1,6 @@
 from nilearn.connectome import GroupSparseCovarianceCV
 from nilearn import datasets
 
-
 from src.process_data.fmri_data import fMRIExperimentData
 from src.process_data.fmri_data import generate_mni_parallel
 from src.process_data.graph_data import generate_graph_parallel
@@ -11,13 +10,21 @@ from src.process_data.graph_data import GraphExperimentData
 if __name__ == '__main__':
 
     dirlist = ['data/sub%03d/'% i for i in range(1, 103)]
+
     fed = fMRIExperimentData(dirlist, working_directory='data/')
+
+    # Generate the MNI files
     generate_mni_parallel(fed)
 
+    # Load the MNI files into the class
+    fed.load_subject_mni()
+
+    # Our graph model -- MSDL Atlas, GroupSparseCovarianceCV
     msdl_atlas_dataset = datasets.fetch_atlas_msdl()
     gsc = GroupSparseCovarianceCV(verbose=2)
 
+    # Use default graph properties
     ged = GraphExperimentData([gsc], fed, msdl_atlas_dataset,
                               msdl_atlas_dataset.maps)
 
-    generate_graph_parallel(ged)
+    ged = generate_graph_parallel(ged)
