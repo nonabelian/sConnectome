40dec5cdcc558cf92f23fc2f71848d2f11d65e54
Dylan Albrecht <dylan.alb@gmail.com>
2016-11-15 13:22:14 -0700

    updating plotting code and images to reflect the preprocessing that happened in the analysis

--- src/visualization/plot_graphs.py
+++ src/visualization/plot_graphs.py
@@ -1,4 +1,5 @@
 from nilearn import image
+from nilearn import input_data
 from nilearn import plotting
 
 
@@ -32,17 +33,32 @@ def plot_connectome2d(graph, show_plot=False, save_image=None):
 def plot_filtered_mni(fmri_task_data, show_plot=False, save_image=None):
 
     _, filtered_mni = fmri_task_data.filtered_mni_image
+    _, c = fmri_task_data.confounds
 
+    masker = input_data.NiftiMasker(detrend=True,
+       low_pass=None, high_pass=0.01, t_r=2.5, standardize=True)
+
+    masker.fit(filtered_mni)
+
+    hv_confounds = image.high_variance_confounds(filtered_mni)
+    img_2darray = masker.transform(filtered_mni, confounds=[hv_confounds, c])
+
+    # Turn into a NIfTI image:
+    pimg = masker.inverse_transform(img_2darray)
+
+    # Grab a frame to plot
     i = 0
-    for img in image.iter_img(filtered_mni):
+    for p1img in image.iter_img(pimg):
         if i == 10:
             break
         i += 1
 
-    display = plotting.plot_stat_map(img, threshold=2000)
+    display = plotting.plot_stat_map(p1img)
     
     if save_image:
         display.savefig(save_image)
 
     if show_plot:
         plotting.show()
+
+
